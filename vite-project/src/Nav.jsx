5fb037542e929c6335735184177136b4c3d235eb
kwanzv <kwanzeev@gmail.com>
2024-02-06 19:57:41 +0700

    Simple rotation animations on nav buttons while hovered and clicked

--- vite-project/src/Nav.jsx
+++ vite-project/src/Nav.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./App.css";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 function NavElement(props) {
   return (
-    <li className="px-4 py-6 md:py-2 rounded-md bg-orange-400 text-black hover:text-white font-semibold ">
+    <motion.button
+      className="px-4 py-6 md:py-2 rounded-md bg-orange-400 text-black hover:text-white font-semibold "
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95, rotate: "2.5deg" }}
+    >
       <a href={props.href} className="hover:underline underline-offset-8 ">
         {props.children}
       </a>
-    </li>
+    </motion.button>
   );
 }
 
@@ -16,7 +23,12 @@ export default function Nav() {
   return (
     <>
       <nav className="flex justify-between py-6 mb-12 container">
-        <span className="text-3xl text-orange-400 font-bold">KwanZV</span>
+        <motion.span
+          className="text-3xl text-orange-400 font-bold"
+          whileTap={{ scale: 0.95, rotate: "5deg" }}
+        >
+          KwanZV
+        </motion.span>
         <ul className="flex gap-4">
           <li>
             <BsFillMoonStarsFill className="cursor-pointer text-3xl" />
